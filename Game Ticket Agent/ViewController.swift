fbeeb64ccf27fccc8e1560ea0b9a67979c8a8955
Parikh Dhruvin S <n01150847@l115-n-013.mediastudies.ca>
2018-04-25 08:34:56 -0400

    Initial Commit

---
+++ Game Ticket Agent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game Ticket Agent
+//
+//  Created by Parikh Dhruvin S. on 4/25/18.
+//  Copyright © 2018 Parikh Dhruvin S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
