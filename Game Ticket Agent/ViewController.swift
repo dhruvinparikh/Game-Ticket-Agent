75468da92a0a27a71bff9dc7821322ff601ef9c6
Dhruvin Parikh <n01150847@humbermail.ca>
2018-04-25 10:05:16 -0400

    Login and navigation working with credentials

--- Game Ticket Agent/ViewController.swift
+++ Game Ticket Agent/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfPassword: UITextField!
+    @IBOutlet weak var tfUsername: UITextField!
+    @IBAction func onLoginBtnClick(_ sender: UIButton) {
+        if(tfUsername.text == "admin" && tfPassword.text == "password"){
+            performSegue(withIdentifier: "toGameTicket", sender: nil)
+        }
+        else{
+            lblError.text="Username and password incorrect"
+        }
+    }
+    @IBOutlet weak var lblError: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
